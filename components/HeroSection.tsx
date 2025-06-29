b7830b28af915b8d7d0f08c7ba286123dda5a1b0
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-06-29 11:18:36 +0530

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <section className="relative w-full h-screen overflow-hidden">
       <video
-        className="absolute top-0 left-0 w-full h-full object-cover"
+        className="absolute top-0 left-0 w-full h-full object-cover scale-[1.3]"
         src="/videos/brajkiholi.webm"
         autoPlay
         muted
